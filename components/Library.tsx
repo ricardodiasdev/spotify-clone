dc48db787911f712411eb80edef752f5ed86e0f3
Ricardo Dias <ricardodias.dev@gmail.com>
2023-06-10 12:08:46 -0300

    Desenvolvendo Library

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,8 +5,14 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import MediaItem from "./MediaItem";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -18,7 +24,7 @@ const Library = () => {
 
     return uploadModal.onOpen();
   };
-  
+
   return (
     <div className="flex flex-col">
       <div
@@ -68,7 +74,13 @@ const Library = () => {
                 px-3
               "
       >
-        List of Songs!
+        {songs.map((item) => (
+          <MediaItem 
+            onClick={() => {}}
+            key={item.id}
+            data={item}
+          />
+        ))}
       </div>
     </div>
   );
