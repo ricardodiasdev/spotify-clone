5d8866f8124e70bb1fe2a388d12bcac67413e396
Ricardo Dias <ricardodias.dev@gmail.com>
2023-06-13 20:53:39 -0300

    Implementando onPlay em Library

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,6 +7,7 @@ import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
 import { Song } from "@/types";
 import MediaItem from "./MediaItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface LibraryProps {
   songs: Song[];
@@ -16,6 +17,8 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
+
+  const onPlay = useOnPlay(songs)
 
   const onClick = () => {
     if (!user) {
@@ -76,7 +79,7 @@ const Library: React.FC<LibraryProps> = ({ songs }) => {
       >
         {songs.map((item) => (
           <MediaItem 
-            onClick={() => {}}
+            onClick={(id) => onPlay(id)}
             key={item.id}
             data={item}
           />
