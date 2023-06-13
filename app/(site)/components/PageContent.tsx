9d8a3f06029831765082435fbfd5dc1b6e15c47e
Ricardo Dias <ricardodias.dev@gmail.com>
2023-06-12 21:13:00 -0300

    Implementando onPlay em PageContent de site

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -2,12 +2,15 @@
 
 import { Song } from "@/types";
 import SongItem from "../../../components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 
 interface PageContentProps{
     songs: Song[]
 }
 
 const PageContent: React.FC<PageContentProps> = ({songs}) => {
+
+    const onPlay = useOnPlay(songs)
     if(songs.length === 0) {
         return (
             <div className="mt-4 text-neutral-400">
@@ -32,7 +35,7 @@ const PageContent: React.FC<PageContentProps> = ({songs}) => {
             {songs.map((item) => (
                 <SongItem 
                   key={item.id}
-                  onClick={() => {}}
+                  onClick={(id: string) => onPlay(id)}
                   data={item}
                 />
             ))}
