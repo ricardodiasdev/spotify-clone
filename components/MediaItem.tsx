698316766ee0ef5965855b2e655c435b40b9ed8e
Ricardo Dias <ricardodias.dev@gmail.com>
2023-06-20 12:54:14 -0300

    Modificando MediaItem e finalizando projeto

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
 import Image from "next/image";
 import React from "react";
@@ -12,12 +13,14 @@ interface MediaItemProps {
 
 const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
   const imageUrl = useLoadImage(data);
+  const player = usePlayer();
 
   const handleClick = () => {
     if (onClick) {
       return onClick(data.id);
     }
-    //default turn on player
+
+    return player.setId(data.id);
   };
 
   return (
